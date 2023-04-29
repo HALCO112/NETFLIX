ae2786d7d1b8ed1ef7775cd1b539e3ed79f675bb
HALCO112 <ajdingta321@gmail.com>
2023-04-29 15:27:36 +0200

    pppp

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const App = () => {
   return (
-    <div class="flex justify-evenly ...">
-      <div>01</div>
-      <div>02</div>
-      <div>03</div>
+    <div className="flex gap-5 justify-center mt-[400px] " >
+      
+      <div className="box-content h-32 w-32 p-4  rounded-3xl text-cyan-50 text-center  bg-slate-900 ">01</div>
+       
     </div>
+    
   );
 };
 
