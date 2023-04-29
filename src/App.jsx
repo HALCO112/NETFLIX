08079aa5c02b347e677f715dba3cc8497bb5a025
HALCO112 <ajdingta321@gmail.com>
2023-04-29 14:02:03 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="flex justify-evenly ...">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+  );
+};
+
+export default App;
